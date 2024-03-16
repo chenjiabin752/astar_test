9533826bace5a372ca459bc33f3aef1147a3a969
ubuntu-cjb <742665406@qq.com>
2024-03-16 21:55:43 +0800

    first commit

---
+++ build/CMakeFiles/astar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/astar"
+  "../bin/astar.pdb"
+  "CMakeFiles/astar.dir/src/a_star.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/astar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readme.txt
@@ -0,0 +1,10 @@
+ 1.编写a_star.cpp
+ 2.编写CMakeLists.txt
+ 3.编译
+ $ mkdir build
+ $ cd build
+ $ cmake ../
+ $ make -j 8
+4.运行可执行文件
+ $ cd bin
+ $ ./astar
